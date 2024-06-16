246702e12519644e61c263b8629ae521bb87496a
Yasu Watanabe <ywata@protonmail.com>
2024-06-17 03:15:01 +0900

    fix: broken test for evalEdgeProp

--- tests/MainTests.elm
+++ tests/MainTests.elm
@@ -47,18 +47,43 @@ test_evalEdgeProp =
     describe "Testing evalEdgeProp function"
         [ test "evalEdgeProp valid" <|
             \_ ->
-                Main.evalEdgeProp [ "0", "1" ]
-                    ([ { name = "prop1"
-                       , rpType = Main.RPIBool
-                       , semantic = Main.Edge
+                Main.evalEdgeProp
+                    ([ { name = "slat"
+                       , rpType = Main.RPFloat
+                       , semantic = Main.Quantity
+                       }
+                     , { name = "slng"
+                       , rpType = Main.RPFloat
+                       , semantic = Main.Quantity
+                       }
+                     , { name = "elat"
+                       , rpType = Main.RPFloat
+                       , semantic = Main.Quantity
+                       }
+                     , { name = "elng"
+                       , rpType = Main.RPFloat
+                       , semantic = Main.Quantity
+                       }
+                     , { name = "dist"
+                       , rpType = Main.RPFloat
+                       , semantic = Main.Quantity
                        }
                      , { name = "prop1"
                        , rpType = Main.RPIBool
                        , semantic = Main.StartNode
                        }
+                     , { name = "prop2"
+                       , rpType = Main.RPIBool
+                       , semantic = Main.Edge
+                       }
+                     , { name = "prop3"
+                       , rpType = Main.RPIBool
+                       , semantic = Main.EndNode
+                       }
                      ]
                         |> List.indexedMap Tuple.pair
                         |> Dict.fromList
                     )
-                    |> Expect.equal [ Just False, Just True ]
+                    [ "10.0", "11.0", "20.0", "21.0", "3.9", "0", "1", "1" ]
+                    |> Expect.equal (Dict.fromList [ ( 6, [ Main.PolyLine [ { lat = 10, lng = 11 }, { lat = 20, lng = 21 } ] ] ), ( 7, [ Main.Point { lat = 20, lng = 21 } ] ) ])
         ]
